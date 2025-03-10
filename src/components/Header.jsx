53e173f0664a92f27e44daceff8c22c08929e96e
Mrinmoy <mrinmoyd629@gmail.com>
2025-03-11 00:58:21 +0600

    font+navigation+main layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.png";
 import moment from "moment";
 
 const Header = () => {
+  const [currentTime, setCurrentTime] = useState(
+    moment().format("MMMM Do YYYY, h:mm:ss a")
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    }, 1000); // Updates every second
+
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center gap-2 py-2">
       <div className="logo">
-        <img className="w-[300px]" src={logo} alt="" />
+        <img className="w-[300px]" src={logo} alt="Logo" />
       </div>
       <h2 className="text-gray-400">Journalism Without Fear or Favour</h2>
-      <p>{moment().format("MMMM Do YYYY, h:mm:ss a")}</p>
+      <p>{currentTime}</p>
     </div>
   );
 };
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
   return (
-    <div>
+    <div style={{ fontFamily: "'Poppins', sans-serif" }}>
       <header>
         <Header></Header>
         <section className="w-11/12 mx-auto">
           <LatestNews></LatestNews>
         </section>
       </header>
-      <nav></nav>
-      <main></main>
+      <nav className="w-11/12 mx-auto py-2">
+        <Navbar></Navbar>
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 grid grid-cols-12">
+        <aside className="left col-span-3">Left Nav</aside>
+        <aside className="col-span-6">Main Content</aside>
+        <aside className="col-span-3">Right Navbar</aside>
+      </main>
     </div>
   );
 };
