107548bfeccb8c31810152231155521a8c5f6955
Mrinmoy <mrinmoyd629@gmail.com>
2025-03-12 23:58:23 +0600

    left navbar+fetch category data

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import Navbar from "../components/Navbar";
+import LeftNavbar from "../components/layout-components/LeftNavbar";
 
 const HomeLayout = () => {
   return (
@@ -16,7 +17,9 @@ const HomeLayout = () => {
         <Navbar></Navbar>
       </nav>
       <main className="w-11/12 mx-auto pt-5 grid grid-cols-12">
-        <aside className="left col-span-3">Left Nav</aside>
+        <aside className="left col-span-3">
+          <LeftNavbar></LeftNavbar>
+        </aside>
         <aside className="col-span-6">Main Content</aside>
         <aside className="col-span-3">Right Navbar</aside>
       </main>
